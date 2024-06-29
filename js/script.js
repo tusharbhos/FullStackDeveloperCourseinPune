0409b211805484ad29e12a06135c2ccd9f954711
tushar bhos <tusharbhos3412@gmail.com>
2024-06-29 17:33:08 +0530

    all chenges complate

--- js/script.js
+++ js/script.js
@@ -10,5 +10,67 @@ let arr = [
     i = (i + 1) % arr.length;
   }, 2000);
 
+  
+// navbar color changing....
+
+document.addEventListener("scroll",()=>{
+  const header=document.getElementsByClassName("header_section");
+  const list=document.getElementsByClassName("navbar-nav");
+  const hamberger=document.getElementsByClassName("navbar-toggler");
+  
+  if(window.scrollY>0){
+    header[0].classList.add("scrolled");
+    list[0].classList.add("list-scroll");
+    hamberger[0].classList.add("navbar-color");
+  }
+  else{
+    header[0].classList.remove("scrolled");
+    list[0].classList.remove("list-scroll");
+    hamberger[0].classList.add("navbar-color");
+  }
+  })
+
+
+
+  // slick slider...................
+  $(document).ready(function(){
+    $('.customer-logos').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 4,
+      autoplay:true,
+      autoplaySpeed:2000,
+      infinite: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  });
+  
+
 
 
